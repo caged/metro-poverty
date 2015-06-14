a99d525496762d08ba6ce7e1b99ee3829932d07e
Justin Palmer <justin@github.com>
2015-06-13 20:15:29 -0700

    use gradient for colors

--- html/js/index.js
+++ html/js/index.js
@@ -3,16 +3,16 @@
   function render(metros) {
     // Basic set up of dimensions and sizes
     var el = d3.select('.js-canvas'),
-       margin = { top: 20, right: 20, bottom: 0, left: 20 },
+       margin = { top: 20, right: 10, bottom: 10, left: 10 },
        width  = parseFloat(el.style('width')) - margin.left - margin.right,
-       height = 1200 //parseFloat(el.style('height')) - margin.top - margin.bottom
+       height = parseFloat(el.style('height')) - margin.top - margin.bottom
 
     // Initialize a grid layout that will allow us to easily implement a
     // small-multiples-style visualization
     var grid = d3.layout.grid()
       .bands()
-      .cols(6)
-      .padding([0.1, 0.1])
+      .cols(9)
+      .padding([0.02, 0.09])
       .size([width, height])
 
     metros = grid(metros)
@@ -21,7 +21,7 @@
         plotWidth  = plotSize[0],
         plotHeight = plotSize[1]
 
-    var plotMargin = { top: 20, right: 0, bottom: 0, left: 0 },
+    var plotMargin = { top: 20, right: 0, bottom: 1, left: 0 },
         plotWidth = plotWidth - plotMargin.left - plotMargin.right
         plotHeight = plotHeight - plotMargin.top - plotMargin.bottom
 
@@ -51,6 +51,7 @@
       .scale(x)
       .tickSize(plotHeight)
       .tickFormat(function(d, i) { return years[i] })
+      .tickPadding(5)
 
     // Initialize a path generator so we can create a path for each tract
     // in a metro area.
@@ -66,45 +67,69 @@
     .append('g')
       .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
 
+    // Crate a linear gradient to use later in our paths
+    vis.append('linearGradient')
+      .attr('id', 'fallen-star-gradient')
+      .attr('gradientUnits', 'userSpaceOnUse')
+      .attr('x1', 0).attr('y1', y(povertyRateMin))
+      .attr('x2', 0).attr('y2', y(povertyRateMax))
+    .selectAll('stop')
+      .data(generateColorStops(10, '#40C90A', '#F14C24'))
+    .enter().append('stop')
+        .attr('offset', function(d) { return d.offset })
+        .attr('stop-color', function(d) { return d.color })
+
     // Create and position a plot for each metro area
     var plot = vis.selectAll('.plot')
       .data(metros)
     .enter().append('g')
       .attr('transform', function(d) { return 'translate(' + d.x + ',' + d.y + ')' })
-      .attr('class', 'plot')
+      .attr('class', function(d, i) { return 'plot ' + 'plot-' + i })
     .append('g')
       .attr('transform', function(d) { return 'translate(' + plotMargin.left + ',' + plotMargin.top + ')' })
+
+    // Create a horizontal divider line
+    plot.append('line')
+      .attr('class', 'divider')
+      .attr('x1', 0)
+      .attr('x2', plotWidth + plotMargin.left + plotMargin.right)
+      .attr('y1', plotHeight + 1)
+      .attr('y2', plotHeight + 1)
+
+    // Draw the x axis ticks and labels
+    plot.append('g')
+      .attr('class', 'axis x')
+      .call(xax)
 
     // Add the metro title to each plot
     plot.append('text')
       .attr('class', 'title')
+      .attr('x', plotWidth / 2)
+      .attr('y', -5)
       .text(formatMetroName)
 
+    // Add a group to house all tract lines for poverty rates
     var rates = plot.append('g')
       .attr('class', 'poverty-rate')
 
+    // Add a group for each tract.
+    // TODO: We could potentially get rid of this extra nesting assuming we
+    // don't put other objects in this group
     var tracts = rates.selectAll('.tract')
       .data(function(d) { return d.values.tracts.sort(function(a, b) { return a.fallenStar - b.fallenStar }) })
     .enter().append('g')
       .attr('class', 'tract')
 
+    // Create a path for each tract based on the poverty since 1970
     tracts.append('path')
       .attr('class', 'rate-line')
       .classed('newly-poor', function(d) { return d.newlyPoor })
       .classed('fallen-star', function(d) { return d.fallenStar })
+      .classed('chronically-poor', function(d) { return d.chronicallyPoor })
       .attr('d', function(d) { return  path(d.povrate) })
-
-    plot.append('g')
-      .attr('class', 'axis x')
-      .call(xax)
-
-    // tracts.selectAll('.rate')
-    //   .data(function(d) { return d.povrate })
-    // .enter().append('circle')
-    //   .attr('class', 'rate')
-    //   .attr('cx', function(d, i) { return x(i) })
-    //   .attr('cy', y)
-    //   .attr('r', 1)
+      .style('stroke', function(d) {
+        if(d.fallenStar) return 'url(#fallen-star-gradient)'
+      })
 
   }
 
@@ -112,6 +137,26 @@
   function formatMetroName(tract) {
     var parts = tract.key.split(',')
     return parts[0].split('-')[0].split('/')[0] + ', ' + parts[1].split('-')[0]
+  }
+
+  function generateColorStops(count, colorFrom, colorTo) {
+    count -= 1
+    var stops = [],
+        cursor = 0,
+        step  = 100 / count,
+        scale = d3.scale.linear()
+          .domain([0, count])
+          .range([colorFrom, colorTo])
+          .interpolate(d3.interpolateHcl)
+
+    for(; cursor <= count; cursor++) {
+      stops.push({
+        offset: Math.ceil(cursor * step) + '%',
+        color: scale(cursor)
+      })
+    }
+
+    return stops
   }
 
   d3.json('/metros.json', render)
